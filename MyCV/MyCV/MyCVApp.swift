effea153f9ff5261aac9743cf58c6d4f01eb1f40
Javier Galera <javier.galera@bestsecret.com>
2021-10-12 13:02:30 +0200

    Initialize repository

---
+++ MyCV/MyCV/MyCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCVApp.swift
+//  MyCV
+//
+//  Created by Galera, Javier on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# My CV App
+
+A simple app to upload your CV. Besides, the app provide an interfaz to the users for watching another CVs (if the owner upload it as public mode).
